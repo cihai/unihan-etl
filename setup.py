c37c1d6c5c8d91f057863a3feeb022ad65ba1966
Tony Narlock <tony@git-pull.com>
2014-01-20 09:29:44 +0800

    initial files for cihaidata.unihan

---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf8 - *-
+"""cihaidata-unihan lives at <https://github.com/tony/cihaidata-unihan>.
+
+cihaidata-unihan
+~~~~~
+
+Unihan abstraction layer.
+
+"""
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+with open('requirements.pip') as f:
+    install_reqs = [line for line in f.read().split('\n') if line]
+    tests_reqs = []
+
+if sys.version_info < (2, 7):
+    install_reqs += ['argparse']
+    tests_reqs += ['unittest2']
+
+import re
+VERSIONFILE = "cihaidata-unihan/__init__.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    __version__ = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+setup(
+    name='cihaidata-unihan',
+    version=__version__,
+    url='https://github.com/cihai/cihaidata-unihan',
+    download_url='https://pypi.python.org/pypi/cihaidata-unihan',
+    license='MIT',
+    author='Tony Narlock',
+    author_email='tony@git-pull.com',
+    description='Unihan dataset for cihai and datapackages.',
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    install_requires=install_reqs,
+    tests_require=tests_reqs,
+    test_suite='cihaidata_unihan.testsuite',
+    zip_safe=False,
+    packages=find_packages(exclude=["doc"]),
+    package_data={
+        'cihaidata-unihan': ['data/*']
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "License :: OSI Approved :: MIT License",
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        "Topic :: Utilities",
+        "Topic :: System :: Shells",
+    ],
+)
