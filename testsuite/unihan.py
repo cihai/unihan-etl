4a5209435011b2673b63246db8cae165179f0f6e
Tony Narlock <tony@git-pull.com>
2014-01-31 07:49:12 +0800

    testsuite.helpers

--- testsuite/unihan.py
+++ testsuite/unihan.py
@@ -25,7 +25,6 @@ from __future__ import absolute_import, division, print_function, \
     with_statement, unicode_literals
 
 import os
-import sys
 import tempfile
 import logging
 import unittest
@@ -37,68 +36,18 @@ try:
 except ImportError:  # Python 2.7
     import unittest
 
-from contextlib import contextmanager
+
 
 from scripts import process
 
 from scripts.process import UNIHAN_URL, UNIHAN_DEST, WORK_DIR, UNIHAN_FIELDS, \
-    UNIHAN_FILES, default_config, Builder, text_type, StringIO, UNIHAN_ZIP_FILEPATH
+    UNIHAN_FILES, default_config, Builder, text_type, UNIHAN_ZIP_FILEPATH
 from scripts.util import merge_dict
+from .helpers import add_to_path, setup_path, get_datapath, captureStdErr
 
 
 log = logging.getLogger(__name__)
 
-
-def add_to_path(path):
-    """Adds an entry to sys.path if it's not already there.  This does
-    not append it but moves it to the front so that we can be sure it
-    is loaded.
-    """
-    if not os.path.isdir(path):
-        raise RuntimeError('Tried to add nonexisting path')
-
-    def _samefile(x, y):
-        if x == y:
-            return True
-        try:
-            return os.path.samefile(x, y)
-        except (IOError, OSError, AttributeError):
-            # Windows has no samefile
-            return False
-    sys.path[:] = [x for x in sys.path if not _samefile(path, x)]
-    sys.path.insert(0, path)
-
-
-def setup_path():
-    script_path = os.path.join(
-        os.path.dirname(__file__), os.pardir, 'scripts'
-    )
-    add_to_path(script_path)
-
-
-def get_datapath(filename):
-
-    return os.path.join(
-        os.path.dirname(__file__), 'fixtures', filename
-    )
-
-
-@contextmanager
-def captureStdErr(command, *args, **kwargs):
-    out, sys.stderr = sys.stderr, StringIO()
-    command(*args, **kwargs)
-    sys.stderr.seek(0)
-    yield sys.stderr.read()
-    sys.stderr = out
-
-
-@contextmanager
-def captureStdOut(command, *args, **kwargs):
-    out, sys.stdout = sys.stderr, StringIO()
-    command(*args, **kwargs)
-    sys.stdout.seek(0)
-    yield sys.stdout.read()
-    sys.stdout = out
 
 
 class TestCase(unittest.TestCase):
