8ea4a91ed212dad10ca7464366fec4f7c81e2539
Tony Narlock <tony@git-pull.com>
2014-02-07 06:38:18 +0800

    add package_metadata.

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ import os
 import sys
 from setuptools import setup, find_packages
 
+sys.path.insert(0, os.getcwd())  # we want to grab this:
+from package_metadata import p
 
 with open('requirements.pip') as f:
     install_reqs = [line for line in f.read().split('\n') if line]
@@ -14,31 +16,25 @@ if sys.version_info < (2, 7):
     install_reqs += ['argparse']
     tests_reqs += ['unittest2']
 
-import re
-VERSIONFILE = "__init__.py"
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    __version__ = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+readme = open('README.rst').read()
+history = open('CHANGES').read().replace('.. :changelog:', '')
 
 setup(
-    name='cihaidata-unihan',
-    version=__version__,
+    name=p.title,
+    version=p.version,
     url='https://github.com/cihai/cihaidata-unihan',
     download_url='https://pypi.python.org/pypi/cihaidata-unihan',
-    license='MIT',
-    author='Tony Narlock',
-    author_email='tony@git-pull.com',
-    description='Unihan dataset for cihai and datapackages.',
-    long_description=open('README.rst').read(),
+    license=p.license,
+    author=p.author,
+    author_email=p.email,
+    description=p.description,
+    long_description=readme,
     include_package_data=True,
     install_requires=install_reqs,
     tests_require=tests_reqs,
     test_suite='testsuite',
     zip_safe=False,
+    keywords=p.title,
     packages=find_packages(exclude=["doc"]),
     package_data={
         'cihaidata-unihan': ['data/*']
