4bb1e8ddf804dfa7cda633f79d12ec3e39a8f483
Tony Narlock <tony@git-pull.com>
2017-05-09 18:28:21 -0500

    remove OrderedDict

--- unihan_tabular/process.py
+++ unihan_tabular/process.py
@@ -6,7 +6,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals, with_statement)
 
 import argparse
-import collections
 import fileinput
 import glob
 import os
