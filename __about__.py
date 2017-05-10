4cdbb7720be18dfc6c12f67a29e57558cc25d7f4
Tony Narlock <tony@git-pull.com>
2017-05-10 18:07:44 -0500

    another readme fix

--- __about__.py
+++ __about__.py
@@ -1,7 +1,7 @@
 __title__ = 'unihan-tabular'
 __package_name__ = 'unihan_tabular'
 __description__ = 'Download and export UNIHAN to Python, CSV, JSON and YAML'
-__version__ = '0.6.1'
+__version__ = '0.6.1b'
 __author__ = 'Tony Narlock'
 __email__ = 'cihai@git-pull.com'
 __license__ = 'MIT'
