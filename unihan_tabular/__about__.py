483091e75bdccbfb3f6c8ac55938d4e70d25f84b
Tony Narlock <tony@git-pull.com>
2017-05-11 09:30:53 -0500

    tag 0.6.3

--- unihan_tabular/__about__.py
+++ unihan_tabular/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'unihan-tabular'
 __package_name__ = 'unihan_tabular'
 __description__ = 'Download and export UNIHAN to Python, CSV, JSON and YAML'
-__version__ = '0.6.2'
+__version__ = '0.6.3'
 __author__ = 'Tony Narlock'
 __email__ = 'cihai@git-pull.com'
 __license__ = 'MIT'
