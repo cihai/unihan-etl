095041e1a9cb7f2350bd42bbf724147405081b73
Tony Narlock <tony@git-pull.com>
2017-05-27 21:02:59 -0500

    comment for assert function

--- unihan_etl/test.py
+++ unihan_etl/test.py
@@ -6,6 +6,15 @@ from __future__ import (absolute_import, division, print_function,
 
 
 def assert_dict_contains_subset(subset, dictionary, msg=None):
+    """Ported assertion for dict subsets in py.test.
+
+    :param subset: needle
+    :type subset: dict
+    :param dictionary: haystack
+    :type dictionary: dict
+    :param msg: message for assert if failure found
+    :type msg: str
+    """
     for key, value in subset.items():
         assert key in dictionary, msg
         assert dictionary[key] == value, msg
