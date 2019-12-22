68c455c625c941f41b521b17bcaa753769177695
Tony Narlock <tony@git-pull.com>
2019-12-22 13:10:50 -0600

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -71,6 +71,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: PyPy',
         "Topic :: Utilities",
         "Topic :: Software Development :: Internationalization",
