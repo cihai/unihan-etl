325fbe628fbe1fd03956050185ac3a0a8878199e
Tony Narlock <tony@git-pull.com>
2022-05-22 13:42:53 -0500

    build(metadata): Update description and add keywords

--- unihan_etl/__about__.py
+++ unihan_etl/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "unihan-etl"
 __package_name__ = "unihan_etl"
-__description__ = "Export UNIHAN to Python, Data Package, CSV, JSON and YAML"
+__description__ = "Export UNIHAN data of Chinese, Japanese, Korean to CSV, JSON or YAML"
 __version__ = "0.13.0"
 __author__ = "Tony Narlock"
 __email__ = "cihai@git-pull.com"
