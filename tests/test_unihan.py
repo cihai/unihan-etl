2750f3b89e0858a4a6bb5c87dd75800abec82465
Tony Narlock <tony@git-pull.com>
2023-06-18 12:22:57 -0500

    test(test_unihan): Fix default test

--- tests/test_unihan.py
+++ tests/test_unihan.py
@@ -360,7 +360,12 @@ def test_set_reduce_fields_automatically_when_only_files_specified() -> None:
 def test_no_args() -> None:
     """Works without arguments."""
 
-    assert DEFAULT_OPTIONS == Packager.from_cli([]).options
+    default_options = DEFAULT_OPTIONS.copy()
+    default_options["destination"] = pathlib.Path(
+        str(default_options["destination"]).format(ext=default_options["format"])
+    )
+
+    assert default_options == Packager.from_cli([]).options
 
 
 def test_cli_plus_defaults(mock_zip_path: pathlib.Path) -> None:
