7a219a49095ed55dc73ce02de663943d8ca4cd77
Tony Narlock <tony@git-pull.com>
2023-12-10 07:18:13 -0600

    doctests(expansion): Add test for _expand_kRSGeneric()

--- src/unihan_etl/expansion.py
+++ src/unihan_etl/expansion.py
@@ -573,7 +573,14 @@ class kRSGenericDict(t.TypedDict):
 
 
 def _expand_kRSGeneric(value: t.List[str]) -> t.List[kRSGenericDict]:
-    """Expand kRSGeneric field."""
+    """Expand kRSGeneric field.
+
+    Examples
+    --------
+    >>> _expand_kRSGeneric(['5.10', "213''.0"])  # doctest: +NORMALIZE_WHITESPACE
+    [{'radical': 5, 'strokes': 10, 'simplified': False},
+    {'radical': 213, 'strokes': 0, 'simplified': False}]
+    """
     pattern = re.compile(
         r"""
         (?P<radical>[1-9][0-9]{0,2})
