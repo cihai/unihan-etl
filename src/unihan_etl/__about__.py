71ae4372abbacc2ea7dada8cdcaff96fe41c9a5f
Tony Narlock <tony@git-pull.com>
2025-11-01 12:04:03 -0500

    Tag v0.38.0 (Drop Python 3.9)

--- src/unihan_etl/__about__.py
+++ src/unihan_etl/__about__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 __title__ = "unihan-etl"
 __package_name__ = "unihan_etl"
 __description__ = "Export UNIHAN data of Chinese, Japanese, Korean to CSV, JSON or YAML"
-__version__ = "0.37.0"
+__version__ = "0.38.0"
 __author__ = "Tony Narlock"
 __email__ = "cihai@git-pull.com"
 __github__ = "https://github.com/cihai/unihan-etl"
